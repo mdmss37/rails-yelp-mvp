c570b7ab292f993147f252a68a66cdb49b5edbb8
kazuya nakajima <uniss010@gmail.com>
2017-10-11 12:09:42 +0900

    pass tests for model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destory 
+  has_many :reviews, dependent: :destroy
+  validates :category, inclusion: { 
+    in: ["chinese", "italian", "japanese", "french", "belgian"],
+    allow_nil: false
+  }
+  validates :name, presence: true
+  validates :address, presence: true
 end
